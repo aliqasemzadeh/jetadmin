40ed55819f60efc8c4c9a2ba4cbc9f152b285d31
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-02-03 14:59:11 +0330

    add some new config.

--- config/jetadmin.php
+++ config/jetadmin.php
@@ -1,18 +1,29 @@
 <?php
 return [
-    'name' => 'JetAdmin',
+    'name' => 'Bootstrap Admin Panel',
     'container-app' => 'container',
     'container-panel' => 'container-fluid',
     'container-admin' => 'container-fluid',
     'admin-prefix-url' => 'admin',
     'panel-prefix-url' => 'panel',
+    'test-mode' => 0,
     'per-page' => 15,
     'verify_code_start' => 101010,
     'verify_code_finish' => 989898,
+    'default-country' => 105,
+
     'home' => [
         'display-carousels' => 1,
         'count-carousels' => 5,
         'display-articles' => 1,
         'count-articles' => 5,
+    ],
+    'modules' => [
+        'user' => true,
+        'user_verify' => true,
+        'user_mobile_verify' => true,
+        'notification' => false,
+        'content' => true,
+        'support' => true,
     ]
 ];
