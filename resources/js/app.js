8e0c5b0714c51fdaba8d4532971b94ff86e95ed3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-02-03 14:56:42 +0330

    add new js files.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,12 @@
-import '@popperjs/core';
-import axios from 'axios';
-window.axios = axios;
+require('./bootstrap');
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+import Clipboard from "@ryangjchandler/alpine-clipboard";
 
-// import Echo from 'laravel-echo';
+Alpine.plugin(Clipboard.configure({
+    onCopy: () => {
+        Swal.fire('Copied.');
+    }
+}));
 
-// window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
-
-import Chart from 'chart.js/auto';
-
-import Swal from 'sweetalert2';
-
-import '@tabler/core';
-
-import '../../vendor/aliqasemzadeh/livewire-bootstrap-modal/resources/js/modals';
-
-import Alpine from 'alpinejs';
-import persist from '@alpinejs/persist'
-
-Alpine.plugin(persist);
-
-window.Alpine = Alpine;
-
-Alpine.start();
+Alpine.plugin(Clipboard);
