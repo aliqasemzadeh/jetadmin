ed1ca82e433f6b78c99eabe7965cdb67b8d3590c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-26 03:27:04 +0330

    Add Flowbite plugin to Tailwind configuration
    The Flowbite plugin has been added to the project within the Tailwind configuration file. This required installing Flowbite as a dependency, referenced in both the package.json and package-lock.json files. Hence, the addition of Flowbite changes in these files.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import flowbite from "flowbite/plugin";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,6 +10,7 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./node_modules/flowbite/**/*.js"
     ],
 
     theme: {
@@ -19,5 +21,5 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography, flowbite],
 };
