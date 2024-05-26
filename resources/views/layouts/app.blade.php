0b06a518b2a7aa42049c39e292b8da58ac3d6ade
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-26 08:55:55 +0330

    Added authentication detection to layout
    In the layouts file, two new Twig conditionals were added: @auth and @guest. These are used to display different UI elements based on whether the user is logged in or not.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,7 @@
             </button>
             <div class="h-full overflow-y-auto bg-gray-50 dark:bg-gray-800">
                 <ul class="space-y-2 font-medium">
+                    @auth
                     <li>
                         <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
@@ -171,6 +172,10 @@
                             <span class="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
                         </a>
                     </li>
+                    @endauth
+                    @guest
+
+                    @endguest
                 </ul>
             </div>
         </div>
