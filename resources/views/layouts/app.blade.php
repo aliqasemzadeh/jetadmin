c5c5fe4e6676f79a4cde1f646a9fbb41d2f0a14b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-27 11:21:22 +0330

    Update UI color scheme and add theme toggle feature
    Updated background color for main content area and navigation for a lighter scheme. In addition, a theme toggle feature has been added to allow users to switch between a dark or light mode. These changes aim to provide a better user experience by increasing contrast and offering customization options.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     </head>
     <body class="font-sans antialiased">
         <!-- navbar component -->
-        <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav class="bg-gray-50 dark:bg-gray-800 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div class="flex flex-wrap items-center justify-between p-4">
                 <button data-drawer-target="drawer-right-example" data-drawer-show="drawer-right-example"
                         data-drawer-placement="right" aria-controls="drawer-right-example"
@@ -61,13 +61,13 @@
         @endif
 
         <!-- Page Content -->
-        <main class="bg-gray-60 dark:bg-gray-800">
+        <main class="bg-white dark:bg-gray-900">
             {{ $slot }}
         </main>
 
-        <!-- drawer component -->
+        <!-- menu drawer component -->
         <div id="drawer-right-example"
-             class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-80 dark:bg-gray-800"
+             class="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-gray-50 w-80 dark:bg-gray-900"
              tabindex="-1" aria-labelledby="drawer-right-label">
             <h5 id="drawer-right-label"
                 class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">
@@ -86,7 +86,7 @@
                 </svg>
                 <span class="sr-only">Close menu</span>
             </button>
-            <div class="bg-gray-50 dark:bg-gray-800">
+            <div class="bg-gray-50 dark:bg-gray-900">
                 <ul class="space-y-2 font-medium">
                     @auth
                     <li>
@@ -176,14 +176,15 @@
                     @guest
 
                     @endguest
+
                 </ul>
             </div>
         </div>
 
 
-        <!-- drawer component -->
+        <!-- user drawer component -->
         <div id="drawer-example"
-             class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800"
+             class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-gray-50 w-80 dark:bg-gray-900"
              tabindex="-1" aria-labelledby="drawer-label">
             <h5 id="drawer-label"
                 class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">
@@ -217,6 +218,11 @@
                     {{ __('Register') }}
                 </a>
             </div>
+                <button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    {{ __('Dark/Light') }}
+                </button>
             @endguest
         </div>
 
