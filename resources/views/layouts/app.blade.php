307b9b371e1b8f53d5bb270f8ae9ac28da43cab3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-05-31 16:54:40 +0330

    Add "Home" link to the navigation menu
    A "Home" link was added to the app's navigation menu. Its UI includes an icon along with the text "Home", and it comes with interactive hover effects. The link is currently a dummy one, its functionality needs further implementation.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,14 @@
             </button>
             <div class="bg-gray-50 dark:bg-gray-900">
                 <ul class="space-y-2 font-medium">
+                    <li>
+                        <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd"/>
+                            </svg>
+                            <span class="ms-3">{{ __('Home') }}</span>
+                        </a>
+                    </li>
                     @auth
                     <li>
                         <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
