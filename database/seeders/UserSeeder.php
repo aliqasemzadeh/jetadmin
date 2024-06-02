41471a8fd749e9c5a72c03317ea920229be766a0
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-02 09:35:48 +0330

    Import Hash facade in UserSeeder
    An import statement has been added to the UserSeeder file in the database seeders directory to include the Hash facade. This facade provides secure Bcrypt hashing for storing user passwords.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
