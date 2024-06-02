7fb9f63eef69523af88ed081698d72f28e62ec29
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-02 09:40:31 +0330

    Update migrations and seeders
    Made modifications to user migration schema by making 'name' column nullable and adding a unique 'username' column. Also, 'Permission' and 'Role' classes were imported in the 'RoleAndPermissionSeeder' and 'Carbon' class in the 'UserSeeder'.

--- database/seeders/RoleAndPermissionSeeder.php
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleAndPermissionSeeder extends Seeder
 {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UserSeeder extends Seeder
 {
