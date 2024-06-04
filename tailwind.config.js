6a0a5df6dc5265675589f9ae114b1ecb5c3cf161
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-04 14:30:22 +0330

    Update default sans-serif font in Tailwind config
    The font family for sans-serif in the Tailwind configuration has been updated to Tahoma from Figtree. This change affects all the components using the sans-serif font.

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Tahoma', ...defaultTheme.fontFamily.sans],
             },
         },
     },
