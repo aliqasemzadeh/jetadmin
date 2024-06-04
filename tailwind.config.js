3c852dc6970bab5c4552f81746a2ccbe87c89515
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-04 10:59:46 +0330

    Update dark mode setting in Tailwind configuration
    The Tailwind CSS configuration has been changed to use the 'media' setting for dark mode instead of the previous 'class' setting. This ensures that the dark mode theme responds to system theme settings.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ import flowbite from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
+    darkMode: 'media',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
