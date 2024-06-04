af2ccfe6842c6bb496679ddbdd853c84b512b104
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-04 15:53:47 +0330

    Add TeamsPermissionMiddleware and update related components
    Added TeamsPermissionMiddleware to handle authentication and authorization related to teams. Updated RoleAndPermissionSeeder to create roles without specifying team_ids. Additionally, made necessary adjustments in app configuration to prioritize this new middleware and updated label in layout from "Teams" to "Team".

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->priority([
+            \App\Http\Middleware\TeamsPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,7 +121,7 @@
                                 <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ __('Users') }}</a>
                             </li>
                             <li>
-                                <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ __('Teams') }}</a>
+                                <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ __('Team') }}</a>
                             </li>
                             <li>
                                 <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ __('Role') }}</a>
