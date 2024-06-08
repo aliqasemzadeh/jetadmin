dc08f780305807c9ce75b893b870d9526d52e725
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-08 10:52:08 +0330

    Add pagination to admin user index
    The WithPagination trait was added to the Livewire admin user index. This provides pagination functionality, improving the management interface for admin users and enhancing the overall user experience.

--- app/Livewire/Admin/User/Index.php
+++ app/Livewire/Admin/User/Index.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\Admin\User;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
     public function render()
     {
         return view('livewire.admin.user.index');
