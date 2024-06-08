d600e651e44d6a4b5a3213c93ad0df5287f04cb5
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-08 11:21:10 +0330

    Fix user pagination in Admin User Index
    The patch corrects the user pagination in the Admin User Index file. Previously, the 'get' method was unnecessarily called on the 'paginate' method, leading to a potential error. This amendment removes the 'get' method, ensuring proper functioning of user pagination.

--- resources/views/livewire/admin/user/index.blade.php
+++ resources/views/livewire/admin/user/index.blade.php
@@ -80,11 +80,9 @@
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
-                            <th scope="col" class="px-4 py-3">Product name</th>
-                            <th scope="col" class="px-4 py-3">Category</th>
-                            <th scope="col" class="px-4 py-3">Brand</th>
-                            <th scope="col" class="px-4 py-3">Description</th>
-                            <th scope="col" class="px-4 py-3">Price</th>
+                            <th scope="col" class="px-4 py-3">{{ __('ID') }}</th>
+                            <th scope="col" class="px-4 py-3">{{ __('Name') }}</th>
+                            <th scope="col" class="px-4 py-3">{{ __('Email') }}</th>
                             <th scope="col" class="px-4 py-3">
                                 <span class="sr-only">Actions</span>
                             </th>
@@ -93,11 +91,9 @@
                         <tbody>
                         @foreach($users as $user)
                         <tr class="border-b dark:border-gray-700">
-                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">Apple iMac 27&#34;</th>
-                            <td class="px-4 py-3">PC</td>
-                            <td class="px-4 py-3">Apple</td>
-                            <td class="px-4 py-3">300</td>
-                            <td class="px-4 py-3">$2999</td>
+                            <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->id }}</th>
+                            <td class="px-4 py-3">{{ $user->first_name . " " . $user->last_name }}</td>
+                            <td class="px-4 py-3">{{ $user->email }}</td>
                             <td class="px-4 py-3 flex items-center justify-end">
                                 <button id="apple-imac-27-dropdown-button" data-dropdown-toggle="apple-imac-27-dropdown" class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
                                     <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
