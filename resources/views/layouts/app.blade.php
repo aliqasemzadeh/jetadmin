6a6819d70a16f979bf15f1d01989abf794793a04
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-14 11:44:28 +0330

    Update header background color in app layout
    The color of the header in the app layout has been updated to provide better visibility and contrast. Specifically, the color was changed from white to gray (for both normal and dark modes) within the "app.blade.php" file.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -219,7 +219,7 @@
         <div class="mt-16">
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-gray-900 dark:bg-gray-400 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
