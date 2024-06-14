23629fb7eeece2a2ba5af8dfdfc3e5ccd615c038
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-14 09:03:54 +0330

    Update package versions and adjust route configuration
    This commit updates multiple dependencies in composer.lock to their latest versions, such as psy/psysh, laravel-lang/config, myclabs/deep-copy, and more. It also adjusts the route configuration in web.php for localization. Changes on .idea files are ignored as they are related to personal IDE settings.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', \App\Livewire\Home\Index::class)->name('home');
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
-    Route::get('/dashboard/index', \App\Livewire\Admin\User\Index::class)->name('dashboard.index');
+Route::group(['prefix' => \Mcamara\LaravelLocalization\Facades\LaravelLocalization::setLocale()], function(){
+    Route::get('/', \App\Livewire\Home\Index::class)->name('home');
 
-    Route::group(['prefix' => config('jetadmin.admin_route_prefix')], function () {
-        Route::get('/dashboard/index', \App\Livewire\Admin\Dashboard\Index::class)->name('admin.dashboard.index');
-        Route::get('/user/index', \App\Livewire\Admin\User\Index::class)->name('admin.user.index');
+    Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+        Route::get('/dashboard/index', \App\Livewire\Admin\User\Index::class)->name('dashboard.index');
+
+        Route::group(['prefix' => config('jetadmin.admin_route_prefix')], function () {
+            Route::get('/dashboard/index', \App\Livewire\Admin\Dashboard\Index::class)->name('admin.dashboard.index');
+            Route::get('/user/index', \App\Livewire\Admin\User\Index::class)->name('admin.user.index');
+        });
     });
-
-
 });
+
