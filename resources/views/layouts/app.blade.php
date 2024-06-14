adfd24664d1f851f7d560c194a53d2a8b492643b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-06-14 10:30:11 +0330

    Update dark mode settings and adjust text color
    The dark mode preference in the TailwindCSS configuration has been switched from 'media' to 'class'. Also, the text color in a hyperlink under dark mode in 'app.blade.php' was changed from white to gray for better visibility and consistency with the dark mode design.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,7 +103,7 @@
                         <ul id="dropdown-example" class="hidden py-2 space-y-2">
 
                             <li>
-                                <a href="{{ route('admin.user.index') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ __('Users') }}</a>
+                                <a href="{{ route('admin.user.index') }}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700">{{ __('Users') }}</a>
                             </li>
                             <li>
                                 <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">{{ __('Team') }}</a>
