7db16f2a0456130ab0c2f960c5bdc161064b2aae
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-03-01 12:58:30 +0330

    Add language files for roles and permissions
    Created `roles.php` and `permissions.php` under the `lang/en` directory. These files define language strings for administrator roles and permissions to improve localization support.

---
+++ lang/en/permissions.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'administrator_access' => 'Administrator Access',
+    'administrator_user_managements' => 'Administrator User Managements',
+
+    'administrator_user_index' => 'Administrator User Index',
+    'administrator_user_create' => 'Administrator User Create',
+    'administrator_user_edit' => 'Administrator User Edit',
+    'administrator_user_delete' => 'Administrator User Delete',
+
+    'administrator_user_role_index' => 'Administrator User Role Index',
+    'administrator_user_role_create' => 'Administrator User Role Create',
+    'administrator_user_role_edit' => 'Administrator User Role Edit',
+    'administrator_user_role_delete' => 'Administrator User Role Delete',
+
+    'administrator_user_permission_index' => 'Administrator User Permission Index',
+    'administrator_user_permission_create' => 'Administrator User Permission Create',
+    'administrator_user_permission_edit' => 'Administrator User Permission Edit',
+    'administrator_user_permission_delete' => 'Administrator User Permission Delete',
+];
