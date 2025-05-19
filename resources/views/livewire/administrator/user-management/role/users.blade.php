047803a515b41b969db74b2c96c7f04233457666
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-05-19 06:34:23 +0330

    Refactor role management views to use Flux modal component
    Replaced custom modal markup with the standardized `<flux:modal>` component across role-related views (users, permissions, create, edit). This change enhances consistency, reusability, and maintainability of the codebase. Updated text and layout to align with the Flux component structure.

--- resources/views/livewire/administrator/user-management/role/users.blade.php
+++ resources/views/livewire/administrator/user-management/role/users.blade.php
@@ -1,16 +1,9 @@
-<div class="p-4 bg-white rounded-lg shadow dark:bg-zinc-800 sm:p-5">
-    <!-- Modal header -->
-    <div class="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-zinc-600">
-        <h3 class="text-lg font-semibold text-zinc-900 dark:text-white">
-            {{ __('jetadmin.users') }}: {{ $role->name }}
-        </h3>
-        <button wire:click="$dispatch('closeModal')" type="button" class="text-zinc-400 bg-transparent hover:bg-zinc-200 hover:text-zinc-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-zinc-600 dark:hover:text-white" data-modal-toggle="updateUserModal">
-            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            <span class="sr-only">Close modal</span>
-        </button>
-    </div>
-
-
+<flux:modal name="administrator.user-management.permission.create.modal" class="md:w-96">
+    <div class="space-y-6">
+        <div>
+            <flux:heading size="lg">{{ __('jetadmin.users') }}: {{ $role->name }}</flux:heading>
+            <flux:text class="mt-2">{{ __('jetadmin.role_users_description') }}</flux:text>
+        </div>
         <div>
             <ul class="divide-y divide-gray-200 dark:divide-gray-700">
                 @foreach($role->users as $user)
@@ -32,6 +25,5 @@
                 @endforeach
             </ul>
         </div>
-
-</div>
-
+    </div>
+</flux:modal>
