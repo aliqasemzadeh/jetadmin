27e762f47e5bc8dd58d709098dd8b48d7b5d0fb3
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-06-20 08:22:34 +0330

    add replay

--- app/Livewire/User/Support/Ticket/View.php
+++ app/Livewire/User/Support/Ticket/View.php
@@ -5,6 +5,7 @@ namespace App\Livewire\User\Support\Ticket;
 use App\Models\Support\Ticket;
 use App\Models\Support\TicketFile;
 use App\Models\Support\TicketReplay;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class View extends Component
@@ -23,6 +24,37 @@ class View extends Component
     {
         $this->ticket = Ticket::findOrFail($id);
         $this->replays = TicketReplay::with(['user', 'files'])->where('ticket_id', $this->ticket->id)->latest()->get();
+    }
+
+    public function replay()
+    {
+        $this->validate();
+
+        $replay = new \App\Models\Support\TicketReplay();
+        $replay->ticket_id = $this->ticket->id;
+        $replay->user_id = auth()->user()->id;
+        $replay->body = $this->body;
+        $replay->ip = request()->ip();
+        $replay->save();
+
+        foreach ($this->files as $file) {
+            $fileRecord = new \App\Models\Support\TicketFile();
+            $fileRecord->title = $file->getClientOriginalName();
+            $fileRecord->file = $file->store('ticket_files');
+            $fileRecord->ticket_id = $this->ticket->id;
+            $fileRecord->ticket_replay_id = $replay->id;
+            $fileRecord->user_id = auth()->user()->id;
+            $fileRecord->save();
+        }
+
+        $this->ticket->updated_at = Carbon::now()->toString();
+        $this->ticket->status = 'user';
+        $this->ticket->save();
+
+        $this->body = null;
+        $this->files = [];
+        $this->replays = TicketReplay::with(['user', 'files'])->where('ticket_id', $this->ticket->id)->latest()->get();
+
     }
 
     public function downloadFile(TicketFile $file)
