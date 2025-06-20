938e8d80699002a3a64a791a9a8d2ad3c5720528
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-06-20 08:19:32 +0330

    create ticket

--- app/Livewire/User/Support/Ticket/View.php
+++ app/Livewire/User/Support/Ticket/View.php
@@ -3,15 +3,37 @@
 namespace App\Livewire\User\Support\Ticket;
 
 use App\Models\Support\Ticket;
+use App\Models\Support\TicketFile;
+use App\Models\Support\TicketReplay;
 use Livewire\Component;
 
 class View extends Component
 {
     public Ticket $ticket;
+    public $body;
+    public $replays;
+    public $files = [];
+
+    protected $rules = [
+        'body' => 'required|string',
+        'files.*' => 'file|max:2048|nullable',
+    ];
+
     public function mount($id): void
     {
         $this->ticket = Ticket::findOrFail($id);
+        $this->replays = TicketReplay::with(['user', 'files'])->where('ticket_id', $this->ticket->id)->latest()->get();
     }
+
+    public function downloadFile(TicketFile $file)
+    {
+        if($file->ticket->user_id != auth()->user()->id) {
+            abort('405');
+        }
+
+        return response()->download(storage_path('/app/'.$file->file));
+    }
+
     public function render()
     {
         return view('livewire.user.support.ticket.view');
