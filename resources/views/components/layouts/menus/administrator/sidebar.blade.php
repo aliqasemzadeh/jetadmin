0298b053c7c8f80d66d8d3e22d9795d42e45dfef
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-06-27 09:50:24 +0330

    Make sidebar nav groups expandable and highlight active routes.

--- resources/views/components/layouts/menus/administrator/sidebar.blade.php
+++ resources/views/components/layouts/menus/administrator/sidebar.blade.php
@@ -10,7 +10,7 @@
     @endcan
 
     @can('administrator_user_management')
-        <flux:navlist.group heading="{{ __('jetadmin.user_management') }}" class="grid">
+        <flux:navlist.group heading="{{ __('jetadmin.user_management') }}" class="grid" expandable :expanded="request()->routeIs('administrator.user-management.*')">
             @can('administrator_user_index')
                 <flux:navlist.item icon="user-group" :href="route('administrator.user-management.user.index')" :current="request()->routeIs('administrator.user-management.user.index')" wire:navigate>{{ __('jetadmin.users') }}</flux:navlist.item>
             @endcan
@@ -23,7 +23,7 @@
         </flux:navlist.group>
     @endcan
         @can('administrator_content_management')
-            <flux:navlist.group heading="{{ __('jetadmin.content_management') }}" class="grid">
+            <flux:navlist.group heading="{{ __('jetadmin.content_management') }}" class="grid" expandable  :expanded="request()->routeIs('administrator.content-management.*')">
                 @can('administrator_content_article_index')
                     <flux:navlist.item icon="document-duplicate" :href="route('administrator.content-management.article.index')" :current="request()->routeIs('administrator.content-management.article.index')" wire:navigate>{{ __('jetadmin.articles') }}</flux:navlist.item>
                 @endcan
@@ -33,14 +33,14 @@
             </flux:navlist.group>
         @endcan
         @can('administrator_support_management')
-            <flux:navlist.group heading="{{ __('jetadmin.support_management') }}" class="grid">
+            <flux:navlist.group heading="{{ __('jetadmin.support_management') }}" class="grid" expandable  :expanded="request()->routeIs('administrator.support-management.*')">
                 @can('administrator_support_ticket_index')
                     <flux:navlist.item icon="rectangle-group" :href="route('administrator.support-management.ticket.index')" :current="request()->routeIs('administrator.support-management.*')" wire:navigate>{{ __('jetadmin.tickets') }}</flux:navlist.item>
                 @endcan
             </flux:navlist.group>
         @endcan
         @can('administrator_setting_management')
-            <flux:navlist.group heading="{{ __('jetadmin.setting_management') }}" class="grid">
+            <flux:navlist.group heading="{{ __('jetadmin.setting_management') }}" class="grid" expandable  :expanded="request()->routeIs('administrator.setting-management.*')">
                 @can('administrator_setting_category_index')
                     <flux:navlist.item icon="rectangle-group" :href="route('administrator.setting-management.category.index')" :current="request()->routeIs('administrator.setting-management.category.index')" wire:navigate>{{ __('jetadmin.categories') }}</flux:navlist.item>
                 @endcan
